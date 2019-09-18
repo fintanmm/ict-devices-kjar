9e702c1f532a6d8942d1f7e01facbb9af5127d58
Fintan MacMahon <fintan.macmahon@gmail.com>
2019-09-18 16:19:49 +0100

    Changed xml to json

--- src/test/java/TestContainers.java
+++ src/test/java/TestContainers.java
@@ -8,22 +8,24 @@ import java.util.List;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
 // import io.restassured.RestAssured.*;
 // import io.restassured.matcher.RestAssuredMatchers.*;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class TestContainers {
 
 
-    public void setUp(String baseURI, String method, List headers) {
-        RestAssured.baseURI = baseURI;
-        Response response = RestAssured.given().header(headers).get();
-    }
+    // public void setUp(String baseURI, String method, List headers) {
+    //     RestAssured.baseURI = baseURI;
+    //     Response response = RestAssured.given().header(headers).get();
+    // }
 
     @Test
     public void testIfContaierIsRunning() {
         RestAssured.baseURI = "http://localhost:8090/rest/server/containers";
-        Response response = RestAssured.given().header("Content-type", "application/json").when().get();
+        Response response = RestAssured.given().accept("application/json").when().get();
       
         assertEquals(200, response.getStatusCode());
         assertTrue(response.asString().contains("SUCCESS"));    
@@ -33,15 +35,15 @@ public class TestContainers {
     public void testContainerIsStarted() {
 
         RestAssured.baseURI = "http://localhost:8090/rest/server/containers";
-        RestAssured.given().header("Content-type", "application/xml").when().get().
-            then().assertThat().body("response.kie-containers.kie-container.@status", equalTo("STARTED"));
+        RestAssured.given().accept("application/json").when().get().
+            then().assertThat().body("result.kie-containers.kie-container[0].status", equalTo("STARTED"));
     }
 
     @Test
     public void testContainerName() {
 
         RestAssured.baseURI = "http://localhost:8090/rest/server/containers/ict-devices-kjar-1_0-SNAPSHOT";
-        RestAssured.given().header("Content-type", "application/xml").when().get().
-            then().assertThat().body("response.@type", equalTo("SUCCESS"));
+        RestAssured.given().accept("application/json").when().get().
+            then().assertThat().body("type", equalTo("SUCCESS"));
     }
 }
